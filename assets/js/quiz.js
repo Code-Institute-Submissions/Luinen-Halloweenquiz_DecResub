df59f308b90285606a57e0fa63e9c51b8c48a677
Luinen <lui.peto@gmail.com>
2022-12-18 19:59:26 +0000

    fix indentation errors

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,81 +1,91 @@
 const quiz = [
-    {quest: 'Which day is Halloween in 2022?',
+  {
+    quest: 'Which day is Halloween in 2022?',
     ans:
-    [ {ans1: 'Monday, 31 October', isCorrect: true},
-    {ans2: 'Sunday, 30 October', isCorrect: false},
-    {ans3: 'Tuesday, 01 November', isCorrect: false},
-    {ans4: 'Wednesday, 02 November',isCorrect: false}]       
-    },
-    {quest: 'What is the name of the pot witches use to cook up their potions?',
+      [{ ans1: 'Monday, 31 October', isCorrect: true },
+      { ans2: 'Sunday, 30 October', isCorrect: false },
+      { ans3: 'Tuesday, 01 November', isCorrect: false },
+      { ans4: 'Wednesday, 02 November', isCorrect: false }]
+  },
+  {
+    quest: 'What is the name of the pot witches use to cook up their potions?',
     ans:
-    [ {ans1: 'Magic cooking pot', isCorrect: false},
-    {ans2: 'Cauldron', isCorrect: true},
-    {ans3: 'Black pot', isCorrect: false},
-    {ans4: 'Wok',isCorrect: false}]
-    },
-    {quest: 'Where does the term "jack o\'lantern" come from?',
+      [{ ans1: 'Magic cooking pot', isCorrect: false },
+      { ans2: 'Cauldron', isCorrect: true },
+      { ans3: 'Black pot', isCorrect: false },
+      { ans4: 'Wok', isCorrect: false }]
+  },
+  {
+    quest: 'Where does the term "jack o\'lantern" come from?',
     ans:
-    [{ans1: 'from a Swedish village', isCorrect: false},
-    {ans2: 'from an Irish folktale', isCorrect: true},
-    {ans3: 'from an Icelandic lake', isCorrect: false},
-    {ans4: 'None of them', isCorrect: false}]
-    }, 
-    {quest: 'When was Halloween first celebrated in the United States?',
+      [{ ans1: 'from a Swedish village', isCorrect: false },
+      { ans2: 'from an Irish folktale', isCorrect: true },
+      { ans3: 'from an Icelandic lake', isCorrect: false },
+      { ans4: 'None of them', isCorrect: false }]
+  },
+  {
+    quest: 'When was Halloween first celebrated in the United States?',
     ans:
-    [{ans1: '1778', isCorrect: false},
-    {ans2: '1840', isCorrect: true},
-    {ans3: '1914', isCorrect: false},
-    {ans4: '1960', isCorrect: false}]
-    },
-    {quest: 'Why did people start dressing up in Halloween costumes?',
+      [{ ans1: '1778', isCorrect: false },
+      { ans2: '1840', isCorrect: true },
+      { ans3: '1914', isCorrect: false },
+      { ans4: '1960', isCorrect: false }]
+  },
+  {
+    quest: 'Why did people start dressing up in Halloween costumes?',
     ans:
-    [{ans1: 'because it is fun', isCorrect: false},
-    {ans2: 'to disguise themselves when the spirits come', isCorrect: true},
-    {ans3: 'to see the otherworld', isCorrect: false},
-    {ans4: 'to repel spirits that they believed came back to Earth', isCorrect: false}]
-    },
-    {quest: 'What vegetable is part of a tradition on the night before Halloween?',
+      [{ ans1: 'because it is fun', isCorrect: false },
+      { ans2: 'to disguise themselves when the spirits come', isCorrect: true },
+      { ans3: 'to see the otherworld', isCorrect: false },
+      { ans4: 'to repel spirits that they believed came back to Earth', isCorrect: false }]
+  },
+  {
+    quest: 'What vegetable is part of a tradition on the night before Halloween?',
     ans:
-    [{ans1: 'Pepper', isCorrect: false},
-    {ans2: 'Cucumber', isCorrect: false},
-    {ans3: 'Cabbage(Kale)', isCorrect: true},
-    {ans4: 'Pumpkin', isCorrect: false}]
-    },
-    {quest: 'Why do some people wear clothes inside out on Halloween?',
+      [{ ans1: 'Pepper', isCorrect: false },
+      { ans2: 'Cucumber', isCorrect: false },
+      { ans3: 'Cabbage(Kale)', isCorrect: true },
+      { ans4: 'Pumpkin', isCorrect: false }]
+  },
+  {
+    quest: 'Why do some people wear clothes inside out on Halloween?',
     ans:
-    [{ans1: 'to see a witch at midnight', isCorrect: true},
-    {ans2: 'to transform into an animal', isCorrect: false},
-    {ans3: 'to save the planet', isCorrect: false},
-    {ans4: 'to make a deal with the devil', isCorrect: false}]
-    },
-    {quest: 'What is the most commercially successful horror movie of all time?',
+      [{ ans1: 'to see a witch at midnight', isCorrect: true },
+      { ans2: 'to transform into an animal', isCorrect: false },
+      { ans3: 'to save the planet', isCorrect: false },
+      { ans4: 'to make a deal with the devil', isCorrect: false }]
+  },
+  {
+    quest: 'What is the most commercially successful horror movie of all time?',
     ans:
-    [{ans1: 'Paranormal Activity', isCorrect: false},
-    {ans2: 'Deliver Us from Evil', isCorrect: false},
-    {ans3: 'Alien', isCorrect: false},
-    {ans4: 'It', isCorrect: true}]
-    },
-    {quest: 'What was used before pumpkins to make jack o\'lanterns?',
+      [{ ans1: 'Paranormal Activity', isCorrect: false },
+      { ans2: 'Deliver Us from Evil', isCorrect: false },
+      { ans3: 'Alien', isCorrect: false },
+      { ans4: 'It', isCorrect: true }]
+  },
+  {
+    quest: 'What was used before pumpkins to make jack o\'lanterns?',
     ans:
-    [{ans1: 'Watermelon', isCorrect: false},
-    {ans2: 'Beetroots', isCorrect: false},
-    {ans3: 'Onions', isCorrect: false},
-    {ans4: 'Potatoes', isCorrect: true}]
-    },
-    {quest: 'What does the black cat symbolize?',
+      [{ ans1: 'Watermelon', isCorrect: false },
+      { ans2: 'Beetroots', isCorrect: false },
+      { ans3: 'Onions', isCorrect: false },
+      { ans4: 'Potatoes', isCorrect: true }]
+  },
+  {
+    quest: 'What does the black cat symbolize?',
     ans:
-    [{ans1: 'Power and strength', isCorrect: false},
-    {ans2: 'Life and death', isCorrect: false},
-    {ans3: 'Mystery and secrecy', isCorrect: true},
-    {ans4: 'Freedom and vanity', isCorrect: false}]
-}
+      [{ ans1: 'Power and strength', isCorrect: false },
+      { ans2: 'Life and death', isCorrect: false },
+      { ans3: 'Mystery and secrecy', isCorrect: true },
+      { ans4: 'Freedom and vanity', isCorrect: false }]
+  }
 ];
 
 var myScore = 0;
-var i=0;
+var i = 0;
 function getNewQuestion(i) {
   const question = document.getElementById('question');
-  question.innerText= quiz[i].quest
+  question.innerText = quiz[i].quest;
 
   const answer1 = document.getElementById('ans1');
   const answer2 = document.getElementById('ans2');
@@ -87,108 +97,109 @@ function getNewQuestion(i) {
   answer3.innerText = quiz[i].ans[2].ans3;
   answer4.innerText = quiz[i].ans[3].ans4;
 
-  const ans1Correctness=quiz[i].ans[0].isCorrect;
-  const ans2Correctness=quiz[i].ans[1].isCorrect;
-  const ans3Correctness=quiz[i].ans[2].isCorrect;
-  const ans4Correctness=quiz[i].ans[3].isCorrect;
+  const ans1Correctness = quiz[i].ans[0].isCorrect;
+  const ans2Correctness = quiz[i].ans[1].isCorrect;
+  const ans3Correctness = quiz[i].ans[2].isCorrect;
+  const ans4Correctness = quiz[i].ans[3].isCorrect;
 
-  var clicked = '';
-  var clickedBtn="";
+  var clicked = "";
+  var clickedBtn = "";
 
   answer1.addEventListener('click', () => {
-    answer1.style.border= '3px solid black';
-    answer2.style.border= 'none';
-    answer3.style.border= 'none';
-    answer4.style.border= 'none';
+    answer1.style.border = '3px solid black';
+    answer2.style.border = 'none';
+    answer3.style.border = 'none';
+    answer4.style.border = 'none';
     clicked = ans1Correctness;
-    clickedBtn=answer1;
-}
-);
-  answer2.addEventListener('click', () => {
-    answer2.style.border= '3px solid black';
-    answer1.style.border= 'none';
-    answer3.style.border= 'none';
-    answer4.style.border= 'none';
-    clicked = ans2Correctness;
-    clickedBtn=answer2;
-}
-);
-  answer3.addEventListener('click', () => {
-    answer3.style.border= '3px solid black';
-    answer2.style.border= 'none';
-    answer1.style.border= 'none';
-    answer4.style.border= 'none';
-    clicked = ans3Correctness;
-    clickedBtn=answer3;
-}
-);
-  answer4.addEventListener('click', () => {
-    answer4.style.border= '3px solid black';
-    answer2.style.border= 'none';
-    answer3.style.border= 'none';
-    answer1.style.border= 'none';
-    clicked = ans4Correctness;
-    clickedBtn=answer4;
+    clickedBtn = answer1;
   }
   );
-
+  answer2.addEventListener('click', () => {
+    answer2.style.border = '3px solid black';
+    answer1.style.border = 'none';
+    answer3.style.border = 'none';
+    answer4.style.border = 'none';
+    clicked = ans2Correctness;
+    clickedBtn = answer2;
+  }
+  );
+  answer3.addEventListener('click', () => {
+    answer3.style.border = '3px solid black';
+    answer2.style.border = 'none';
+    answer1.style.border = 'none';
+    answer4.style.border = 'none';
+    clicked = ans3Correctness;
+    clickedBtn = answer3;
+  }
+  );
+  answer4.addEventListener('click', () => {
+    answer4.style.border = '3px solid black';
+    answer2.style.border = 'none';
+    answer3.style.border = 'none';
+    answer1.style.border = 'none';
+    clicked = ans4Correctness;
+    clickedBtn = answer4;
+  }
+  );
   const check = document.getElementsByClassName('check')[0];
-  check.addEventListener("click", () => {
-    if (clicked){
+  check.addEventListener('click', () => {
+    if (clicked) {
       check.style.background = 'green';
-      clickedBtn.style.background= 'green';
+      clickedBtn.style.background = 'green';
     }
     else {
       check.style.background = 'red';
-      clickedBtn.style.background= 'red';
+      clickedBtn.style.background = 'red';
+    }
+    setAnswerButtons(true);
   }
-  setAnswerButtons(true);
+  );
 }
-);
-getNewQuestion(i)
 
-function getNextQuestion(){
-  const next= document.getElementsByClassName('next')[0];
+getNewQuestion(i);
+
+function getNextQuestion() {
+  const next = document.getElementsByClassName('next')[0];
   next.addEventListener('click', () => {
-    resetButtonsStyles()
+    resetButtonsStyles();
     setAnswerButtons(false);
-    if (i<9){
+    if (i < 9) {
       i++;
-      }
+    }
     getNewQuestion(i);
-}
-);
+  }
+  );
 }
 getNextQuestion();
 
-function setAnswerButtons(condition){
+function setAnswerButtons(condition) {
   const answer1 = document.getElementById('ans1');
   const answer2 = document.getElementById('ans2');
   const answer3 = document.getElementById('ans3');
   const answer4 = document.getElementById('ans4');
-  const check = document.getElementsByClassName('check')[0]; 
-  if (condition){
-    answer1.disabled=true;
-    answer2.disabled=true;
-    answer3.disabled=true;
-    answer4.disabled=true;
-    check.disabled=true;
+  const check = document.getElementsByClassName('check')[0];
+  if (condition) {
+    answer1.disabled = true;
+    answer2.disabled = true;
+    answer3.disabled = true;
+    answer4.disabled = true;
+    check.disabled = true;
   }
   else {
-    answer1.disabled=false;
-    answer2.disabled=false;
-    answer3.disabled=false;
-    answer4.disabled=false;
-    check.disabled=false;
+    answer1.disabled = false;
+    answer2.disabled = false;
+    answer3.disabled = false;
+    answer4.disabled = false;
+    check.disabled = false;
   }
 }
 
-function resetButtonsStyles(){
+function resetButtonsStyles() {
   const answer1 = document.getElementById('ans1');
   const answer2 = document.getElementById('ans2');
   const answer3 = document.getElementById('ans3');
   const answer4 = document.getElementById('ans4');
-  const check = document.getElementsByClassName('check')[0]; 
+  const check = document.getElementsByClassName('check')[0];
   answer1.style.background = '#9415b0';
   answer2.style.background = '#9415b0';
   answer3.style.background = '#9415b0';
@@ -200,12 +211,12 @@ function resetButtonsStyles(){
   answer4.style.border = 'none';
 }
 
-function setMyScore(){
+function setMyScore() {
   const correctScore = 5;
   const wrongScore = 1;
   const currentScore = document.getElementById('score')
   const check = document.getElementsByClassName('check')[0];
-  if (clicked){
+  if (clicked) {
     myScore += correctScore;
   }
   else {
